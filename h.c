54655728d779e00657207169fe78e62900f8a1ae
samurollie <53665491+samurollie@users.noreply.github.com>
2020-01-05 18:37:19 -0300

    Arrumei get_frequency

--- h.c
+++ h.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef long long int lli;
 
 lli *get_frequency(FILE *file) {
-    lli repeticao[256] = {0};
+    lli *repeticao = (lli*) malloc(sizeof(lli) * 256);
+    memset(repeticao, 0, 256);
     unsigned char unit;
 	while (fscanf(file,"%c",&unit) != EOF) { // vai até o EOF.
         int a = (int) unit;
