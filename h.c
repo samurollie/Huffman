d9cafd6705047101688eb47d2e369c57a3139767
samurollie <53665491+samurollie@users.noreply.github.com>
2020-01-05 17:59:10 -0300

    Finishing Frequency count

--- h.c
+++ h.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    FILE *arq; 
-    arq = fopen("a.txt", "r");
-    unsigned char inut;
-    long long int repeticao[256] = {0};
-    int i;
-	while (!feof(arq)) { // vai até o EOF.
-  		fscanf(arq,"%c",&inut);
-        int a = (int) inut;
-        // printf ("%d\n", a);
+typedef long long int lli;
+
+lli *get_frequency(FILE *file) {
+    lli repeticao[256] = {0};
+    unsigned char unit;
+	while (fscanf(file,"%c",&unit) != EOF) { // vai até o EOF.
+        int a = (int) unit;
         repeticao[a] += 1;
   	}
-//     for(i = 0; i < 256; i++) {
-//         if(repeticao[i] != 0) {
-//             printf(">>>%lld\n", repeticao[i]);
-//         }
-//     }
+    return repeticao;
+}
+
+FILE *open_file(char* path) {
+    FILE *f = fopen(path, "r");
+    return f;
+}
+
+int main() {
+    FILE *arq; 
+    arq = open_file("a.txt");
+    lli *frequency = get_frequency(arq);
+    
     return 0;
 }
