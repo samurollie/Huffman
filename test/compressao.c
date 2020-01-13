ac3d29f3d40e0d0c8274f8631fd2f32a373435ef
samurollie <53665491+samurollie@users.noreply.github.com>
2020-01-13 02:37:13 -0300

    FUNCIONAAAAA :)

--- test/compressao.c
+++ test/compressao.c
@@ -20,32 +20,39 @@ void search_huff_tree (node* tree, unsigned char item, int path[], int i, int* f
       *found = 1;
 		return;
     }
+  	if(*found != 1) {
+      path[i] = 0;
+	} 	
+	search_huff_tree(tree->left, item, path, i - 1, found);
     if(*found != 1){
       path[i] = 1;
     }
-  	search_huff_tree(tree->right, item, path, i++, found);
-    if(*found != 1) {
-      path[i] = 0;
-    } 	
-  	search_huff_tree(tree->left, item, path, i++, found);
+  	search_huff_tree(tree->right, item, path, i - 1, found);
 }
 
 void dec_to_bin(int beggin, int rest, int array[], int i) { // passar resto = 0;
-  if(beggin >= 1) { // ?????????? ke???
-    rest = beggin % 2;
-    beggin = beggin / 2;
-    // printf ("i ::: %d\n", i);
-    dec_to_bin(beggin, 0, array, i++);
-    printf ("%d", rest);
-    array[i] = rest;
-  }
+	if(beggin >= 1) { // ?????????? ke???
+    	rest = beggin % 2;
+    	beggin = beggin / 2;
+    	// printf ("i ::: %d\n", i);
+    	dec_to_bin(beggin, 0, array, i+1);
+    	printf ("%d", rest);
+    	array[i] = rest;
+	}
+}
+
+int is_leaf(node *tree) {
+  return !tree->left && !tree->right;
 }
 
 void get_number_of_nodes(node* tree, int* size) { // vai contar o numero de nós da arvore.
 	if (tree == NULL) {
 		return;
     }
-  	(*size)++;
+  	if ( is_leaf(tree) == 1 && (tree -> item == '*' || tree -> item == '\\' )) {
+      	(*size)++;
+    }
+    (*size)++;
   	get_number_of_nodes(tree->left, size);
   	get_number_of_nodes(tree->right, size);
 }
@@ -57,7 +64,7 @@ int changed_positions(int array[], int size) {
 			total++;
         }
     }
-  return total;
+	return total;
 }
 
 void print_bits(FILE *file, FILE *compressed_file, hash_table *mapping) {
@@ -81,6 +88,18 @@ void print_bits(FILE *file, FILE *compressed_file, hash_table *mapping) {
           	fprintf(compressed_file, "%c", c);
         }
   	}
+}
+
+int get_trash_size(hash_table *mapping) {
+	int i;
+  	int bits_usados = 0;
+  	for (i = 0; i < HASH_SIZE; i++) {
+		if (mapping->table[i] != NULL) { // Tenho que multiplicar quantos bits um unico caracter daquele ocupa pela quantidade de vezes que ele aparece no texto
+          bits_usados += changed_positions(mapping->table[i]->new_mapping, 8);
+          bits_usados *= mapping->table[i]->frequency;
+        }
+    }
+  	return (8 - (bits_usados % 8) == 8 ? 0 : 8 - (bits_usados % 8));
 }
 
 void get_frequency(FILE *file, hash_table *mapping) {
@@ -137,7 +156,7 @@ int main() {
           	memset(path, -1, sizeof(path));
           	unsigned char item = mapping->table[i]->key;
           	int found = 0;
-		  	search_huff_tree(huff_tree, item, path, 0, &found);	
+		  	search_huff_tree(huff_tree, item, path, 8, &found);	
           	printf ("O caminho para o %c: ", item);
             for (int x = 0; x < 8; x++) {
                 printf ("%d ", path[x]);
@@ -149,14 +168,14 @@ int main() {
 
   	//agora vamos escrever todos os bytes no arquivo, bit a bit;
   	int tree_size = 0;
-  	get_number_of_nodes(huff_tree, &tree_size);
+  	get_number_of_nodes(huff_tree, &tree_size); // PROBLEMA AQUIIIII
   	printf ("Tamanho da árvore: %d\n", tree_size);
 
-    int trash_size = 16 - tree_size;
+    int trash_size = get_trash_size(mapping); // NÃO FUNCIONA !!
     printf("Tamanho do lixo: %d\n", trash_size);
 
   	int array[16];
-  	memset(array, 0, sizeof(array));
+  	memset(array, 0, sizeof(array));  // 
     
     printf ("Tamanho da árvore em binario: ");
   	dec_to_bin(tree_size, 0, array, 0);
@@ -167,29 +186,29 @@ int main() {
     printf ("\n");
   	
     printf("Primeiros bits do arquivo: ");
-    for (int i = 0; i < 16; i++) {
+    /* for (int i = 1;  < 16; i++) {
         if (i == 8) printf(" ");
         printf ("%d", array[i]);
-    }
+    } */
     printf ("\n");
   	
-      unsigned char c = 0, d = 0;	
-  	for (int i = 0; i < 16; i++) {
-		if (i < 8) { // Salvo em um dos bytes;
+    unsigned char c = 0, d = 0;
+  	for (int i = 16 - 1; i >= 0; i--) {
+		if (i >= 8) { // Salvo em um dos bytes;
 			if (array[i] == 1) {
-				c = set_bit(c, i);
+				c = set_bit(c, i - 8);
             }
         } else { // Salva no outro byte;
 			if (array[i] == 1) {
-				d = set_bit(d, i - 8);
+				d = set_bit(d, i);
             }
         }
     }
 
-  	fprintf(compressed_file, "%c", d);
   	fprintf(compressed_file, "%c", c);
+  	fprintf(compressed_file, "%c", d);
     print_on_file(compressed_file, huff_tree); // função que imprimi a arvore no arquivo.
-	print_bits(arq, compressed_file, mapping);
-  	
+	print_bits(arq, compressed_file, mapping); 
+  
     return 0;
 }
