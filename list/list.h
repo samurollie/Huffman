a70235ea0009c8ce925036ac0e96a874fe29e641
eireneof <52976035+eireneof@users.noreply.github.com>
2020-01-22 15:25:44 -0300

    início da documentação

--- list/list.h
+++ list/list.h
@@ -21,12 +21,20 @@ struct list {
     int size;
 };
 
+/** @param head: a cabeça da lista a ser imprimida */
 void print_list (node *head);
 
+/** @param list:      lista que receberá um novo elemento*/
+/** @param new_node:  nó a ser inserido na lista*/
 void insert(list* list, node* new_node);
 
+/** @param list:      lista que receberá um novo elemento*/
+/** @param item:      o item que vai ser salvo na lista */
+/** @param frequency: a frequencia do item*/
+/** @return retorna um novo ponteiro para a lista*/
 list *add (list *list, unsigned char item, lli frequency);
 
+/** @return retorna uma lista vazia */
 list* create_list();
 
 #endif
