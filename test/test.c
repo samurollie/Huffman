0b7e251ef543fcf5513ea2883af0707c83d2b340
samurollie <53665491+samurollie@users.noreply.github.com>
2020-01-24 16:49:36 -0300

    Tirando repetidos

--- test/test.c
+++ test/test.c
@@ -33,14 +33,23 @@ void test_queue(void) {
     CU_ASSERT_PTR_NOT_NULL(queue); // verifica se a queuea não é NULL.
     CU_ASSERT(queue->size == 0); // verifica se o tamanho é zero.
 
-    add(queue, 'A', 6); // nessa função criamos um nó e enfileiramos.
+
+    /*VEFICA SE ADICIONA CERTO ::::: */
+    add(queue, 'A', 6); // nessa função criamos um nó e enfileiramos ordenadamente.
+    CU_ASSERT(queue->head->item == 'A');
     add(queue, 'B', 5);
+    CU_ASSERT(queue->head->item == 'B');
     add(queue, 'C', 4);
+    CU_ASSERT(queue->head->item == 'C');
     add(queue, 'D', 3);
+    CU_ASSERT(queue->head->item == 'D');
     add(queue, 'E', 2);
+    CU_ASSERT(queue->head->item == 'E');
     add(queue, 'F', 1);
+    CU_ASSERT(queue->head->item == 'F');
     
-    CU_ASSERT(queue->head == dequeue(queue)); // verificamos se o nó foi desenfileirado da cabeça.
+    /*VEFICA SE DESENFILEIRA CERTO ::::: */
+    CU_ASSERT(queue->head == dequeue(queue));
     CU_ASSERT(queue->head == dequeue(queue));
     CU_ASSERT(queue->head == dequeue(queue));
     CU_ASSERT(queue->head == dequeue(queue));
@@ -51,7 +60,6 @@ void test_queue(void) {
 int main () {
     CU_pSuite pSuite = NULL; // cria a variavel do tipo CU_pSuite.
 
-
     /* Se essa função não conseguir inicializar, retorna um erro.
     Caso consiga ela retorna a variavel CUE_SUCCESS. */
     if (CU_initialize_registry() != CUE_SUCCESS) {
@@ -59,7 +67,6 @@ int main () {
     }
 
     pSuite = CU_add_suite("Basic_Test_Suite1", init_suite, clean_suite); // cria a suite.
-
     if (pSuite == NULL) {
         CU_cleanup_registry();
         return CU_get_error();
@@ -67,64 +74,6 @@ int main () {
 
     /* Se suite fosse uma fila, poderia dizer que a função
     CU_add_test enfileira um caso de teste */
-
-    if ((CU_add_test(pSuite, "\n\n... Testing Queue Functions...\n\n", test_queue) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    if((CU_add_test(pSuite, "\n\n... Testing Tree...\n\n", test_tree) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-    pSuite = CU_add_suite("Basic_Test_Suite1", init_suite, clean_suite); // cria a suite.
-
-    if (pSuite == NULL) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    /* Se suite fosse uma fila, poderia dizer que a função
-    CU_add_test enfileira um caso de teste */
-
-    if ((CU_add_test(pSuite, "\n\n... Testing Queue Functions...\n\n", test_queue) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    if((CU_add_test(pSuite, "\n\n... Testing Tree...\n\n", test_tree) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-    pSuite = CU_add_suite("Basic_Test_Suite1", init_suite, clean_suite); // cria a suite.
-
-    if (pSuite == NULL) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    /* Se suite fosse uma fila, poderia dizer que a função
-    CU_add_test enfileira um caso de teste */
-
-    if ((CU_add_test(pSuite, "\n\n... Testing Queue Functions...\n\n", test_queue) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    if((CU_add_test(pSuite, "\n\n... Testing Tree...\n\n", test_tree) == NULL)) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-    pSuite = CU_add_suite("Basic_Test_Suite1", init_suite, clean_suite); // cria a suite.
-
-    if (pSuite == NULL) {
-        CU_cleanup_registry();
-        return CU_get_error();
-    }
-
-    /* Se suite fosse uma fila, poderia dizer que a função
-    CU_add_test enfileira um caso de teste */
-
     if ((CU_add_test(pSuite, "\n\n... Testing Queue Functions...\n\n", test_queue) == NULL)) {
         CU_cleanup_registry();
         return CU_get_error();
